bb9d250f4c766dd086c522a8ddeddbf09655d938
M4TV1K <matvikfake@gmail.com>
2019-10-01 12:18:46 +0200

    Creating version control before big changes

---
+++ Scripts/index.js
@@ -0,0 +1,127 @@
+let currentSlide = 1, liveAnimation = false;
+const amountSlides = 6, animationSpeed = 500, time = 100;
+
+window.onload = () => {
+    //todo remove comments and add 'hidden' attr where it's needed
+    /*add light-up to order btn*/
+    $('.makeOrder').mouseover(() => {
+        $('.lightUp').css('color', 'black');
+        $('.smallArrowImg').css('opacity', '1');
+    }).mouseout(() => {
+        $('.lightUp').css('color', 'white');
+        $('.smallArrowImg').css('opacity', '0.75');
+    });
+    /*-------------------------*/
+    /*navigational arrows*/
+    $('#leftArrow').click(prevSlide);
+    $('#rightArrow').click(nextSlide);
+    /*-------------------*/
+
+    /*sound button*/
+    const $soundBtn = $('#soundBtn');
+    $soundBtn.click(() => {
+        console.log($soundBtn.css('background-image'));
+        if ($soundBtn.css('background-image').toString().includes('mute.svg'))
+            $soundBtn.css('background-image', 'url(../ArteDante/Assets/nav-menu/sound.svg)');
+        else
+            $soundBtn.css('background-image', 'url(../ArteDante/Assets/nav-menu/mute.svg)');
+    });
+    /*------------*/
+    /*social media buttons*/
+    $('#instagram').click(() => {
+        window.open("https://www.instagram.com/rtdnt/", '_blank');
+    });
+    /*--------------------*/
+
+    /*setTimeout(() => {
+        $('#loading').fadeOut();
+        $('#nav-menu').fadeIn();
+        $('#backgroundVideo').fadeIn();
+        $('#slides').fadeIn();
+    }, 500);*/
+};
+
+//todo assign this function to onmousewheel after loading
+window.onmousewheel = (evt) => {
+    console.log(liveAnimation + " " + currentSlide + "/" + amountSlides);
+    console.log(evt.deltaX + ', ' + evt.deltaY);
+    if (evt.deltaY > 1) nextSlide();
+    else if (evt.deltaY < -1) prevSlide();
+};
+
+function toTheFirstSlide(slide) {
+    setTimeout(() => {
+        $('#slide' + slide).animate({
+            left: '100%',
+        }, animationSpeed);
+        if (slide > 2) toTheFirstSlide(slide - 1);
+        else {
+            $('#currentSlide').text('01');
+            liveAnimation = false;
+        }
+    }, time);
+}
+
+function nextSlide() {
+    if (!liveAnimation) {
+        liveAnimation = true;
+        if (currentSlide !== amountSlides) {
+            ++currentSlide;
+            $('#slide' + currentSlide).animate({
+                left: 0
+            },{
+                duration: animationSpeed,
+                complete: () => {
+                    $('#currentSlide').text('0' + currentSlide);
+                    liveAnimation = false;
+                }
+            });
+        }
+        else {
+            currentSlide = 1;
+            $('#slide' + amountSlides).animate({
+                left: '100%'
+            }, animationSpeed);
+            toTheFirstSlide(amountSlides - 1);
+        }
+    }
+}
+
+function toTheLastSlide(slide = 3) {
+    console.log('Scrolling slide: ' + 3);
+    setTimeout(() => {
+        $('#slide' + slide).animate({
+            left: 0
+        }, animationSpeed);
+        if (slide < amountSlides) toTheLastSlide(slide + 1);
+        else {
+            $('#currentSlide').text('06');
+            liveAnimation = false;
+        }
+    }, time);
+}
+
+function prevSlide() {
+    if(!liveAnimation) {
+        liveAnimation = true;
+        if (currentSlide !== 1) {
+            $('#slide' + currentSlide).animate({
+                left: '100%'
+            },{
+                duration: animationSpeed,
+                complete: () => {
+                    $('#currentSlide').text('0' + currentSlide);
+                    liveAnimation = false
+                }
+            });
+            --currentSlide;
+        }
+        else {
+            currentSlide = amountSlides;
+            $('#slide2').animate({
+                left: 0
+            }, animationSpeed);
+            toTheLastSlide();
+        }
+    }
+}
